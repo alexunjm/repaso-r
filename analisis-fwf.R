1fa91a8915f7e86121e69f63feb8a21394f6a3b6
alexun <alexunjm@gmail.com>
2018-04-22 15:50:51 -0500

    archivos con columnas de ancho fijo

---
+++ analisis-fwf.R
@@ -0,0 +1,9 @@
+students.data <- read.fwf("repaso-r/data/student-fwf.txt",
+                             widths = c(4,15,20,15,4),
+                             col.names = c("id", "nombre", "email", "carrera", "año")
+                             )
+students.data_header <- read.fwf("https://raw.githubusercontent.com/joanby/r-course/master/data/tema1/student-fwf-header.txt",
+                             widths = c(4,15,20,15,4),
+                             header = T, sep = '\t',
+                             skip = 2
+                             )
