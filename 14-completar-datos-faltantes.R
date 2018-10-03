d1bbbcca0b80b5ae0c2a55b646edcb65bad51b19
alex jaramillo <alexunjm@gmail.com>
2018-10-03 18:31:47 -0500

    definir mediana o un valor fijo

--- 14-completar-datos-faltantes.R
+++ 14-completar-datos-faltantes.R
@@ -9,3 +9,15 @@ housing.data.copy1$ptratio <- impute(housing.data.copy1$ptratio, mean)
 housing.data.copy1$rad <- impute(housing.data.copy1$rad, mean)
 
 summary(housing.data.copy1)
+
+housing.data.copy2 <- housing.data
+housing.data.copy2$ptratio <- impute(housing.data.copy2$ptratio, median)
+housing.data.copy2$rad <- impute(housing.data.copy2$rad, median)
+
+summary(housing.data.copy2)
+
+housing.data.copy3 <- housing.data
+housing.data.copy3$ptratio <- impute(housing.data.copy3$ptratio, 18)
+housing.data.copy3$rad <- impute(housing.data.copy3$rad, 7)
+
+summary(housing.data.copy3)
